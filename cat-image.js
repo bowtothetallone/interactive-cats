a85a72062eba8acdec72cae3003d6b5760d488b7
bowtothetallone <kathy@carlston.tv>
2019-06-21 01:34:00 -0600

    Cleaned up cat design a bit

--- cat-image.js
+++ cat-image.js
@@ -17,53 +17,56 @@ canvasRenderingContext.lineTo(200, 200)
 
 canvasRenderingContext.moveTo(250, 360)
 canvasRenderingContext.lineTo(275, 360)
-canvasRenderingContext.lineTo(250, 200)
+canvasRenderingContext.lineTo(235, 200)
 
 //BackLegs
+canvasRenderingContext.moveTo(360, 360)
+canvasRenderingContext.lineTo(385, 360)
+canvasRenderingContext.lineTo(425, 200)
+
 canvasRenderingContext.moveTo(475, 360)
 canvasRenderingContext.lineTo(500, 360)
-canvasRenderingContext.lineTo(450, 200)
+canvasRenderingContext.lineTo(460, 200)
 
-canvasRenderingContext.moveTo(375, 360)
-canvasRenderingContext.lineTo(400, 360)
-canvasRenderingContext.lineTo(425, 200)
+//neck
+canvasRenderingContext.moveTo(185, 120)
+canvasRenderingContext.lineTo(200, 190)
 
 canvasRenderingContext.stroke()
 
 //Tail
 canvasRenderingContext.beginPath()
-canvasRenderingContext.ellipse(475, 120, 75, 35, Math.PI * .50, 0, Math.PI, true);
+canvasRenderingContext.ellipse(470, 125, 75, 35, Math.PI * .50, 0, Math.PI, true);
 canvasRenderingContext.stroke()
 
 //Ears
 canvasRenderingContext.beginPath()
-canvasRenderingContext.moveTo(110, 65)
-canvasRenderingContext.lineTo(125, 30)
-canvasRenderingContext.lineTo(140, 50)
+canvasRenderingContext.moveTo(125, 105)
+canvasRenderingContext.lineTo(140, 55)
+canvasRenderingContext.lineTo(160, 80)
+canvasRenderingContext.lineTo(185, 55)
+canvasRenderingContext.lineTo(192, 105)
 
-canvasRenderingContext.moveTo(180, 75)
-canvasRenderingContext.lineTo(175, 30)
-canvasRenderingContext.lineTo(155, 50)
 
 canvasRenderingContext.stroke()
 canvasRenderingContext.fill()
-
-//neck
-canvasRenderingContext.beginPath()
-canvasRenderingContext.moveTo(185, 120)
-canvasRenderingContext.lineTo(200, 190)
-canvasRenderingContext.stroke()
 
 //head
-canvasRenderingContext.fillStyle = 'orange'
 canvasRenderingContext.beginPath()
-canvasRenderingContext.ellipse(135, 110, 50, 60, Math.PI / 4, 0, 2 * Math.PI)
+canvasRenderingContext.ellipse(152, 120, 50, 40, Math.PI, 0, 2 * Math.PI)
 canvasRenderingContext.fill()
 
+//default body
+canvasRenderingContext.beginPath()
+canvasRenderingContext.moveTo(200, 200)
+canvasRenderingContext.lineTo(465, 200)
+canvasRenderingContext.stroke() 
+
+//chonky body
 var weight = 0
 var nom = document.getElementById('nomNumber')
 
-function getFat(){
+function chonk(){
     canvasRenderingContext.beginPath()
     canvasRenderingContext.ellipse(332, 200, weight, 130, Math.PI / 2, 0, 2 * Math.PI)
     canvasRenderingContext.fillStyle = myColor
@@ -73,12 +76,5 @@ function getFat(){
 }
 nom.addEventListener("keyup", function(){
     weight=this.value
-    getFat()
+    chonk()
 }, false)
-
-
-//default body
-canvasRenderingContext.beginPath()
-canvasRenderingContext.moveTo(200, 200)
-canvasRenderingContext.lineTo(465, 200)
-canvasRenderingContext.stroke() 
