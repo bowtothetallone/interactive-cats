e1497f44a9a700b5dd708b09ae0db2c4ef22f849
bowtothetallone <kathy@carlston.tv>
2019-06-28 22:48:19 -0600

    cats are generated based on array length, all cats get fat now

--- cat-image.js
+++ cat-image.js
@@ -2,43 +2,23 @@ var nom = document.getElementById('nomNumber')
 var canvas = document.getElementById('cat-canvas')
 var context = canvas.getContext('2d')
 
-var cats = [
-  new Cat({
-    name: 'Grover',
-    color: 'red',
-    x: 100,
-    y: 100,
-    scale: 0.3
-  }),
-  new Cat({
-    name: 'Baxter',
-    color: 'orange',
-    x: 200,
-    y: 100,
-    scale: 0.4
-  }),
-  new Cat({
-    name: 'Bruce',
-    color: 'yellow',
-    x: 300,
-    y: 100,
-    scale: 0.5
-  }),
-  new Cat({
-    name: 'Janet',
-    color: 'green',
-    x: 400,
-    y: 100,
-    scale: 0.6
-  }),
-  new Cat({
-    name: 'Admiral',
-    color: 'Blue',
-    x: 500,
-    y: 100,
-    scale: 0.7
-  })
+var catNames = [
+  'Grover',
+  'Baxter',
+  'Bruce',
+  'Janet',
+  'Admiral'
 ]
+
+var cats = catNames.map(function (catName, index) {
+  return new Cat({
+    name: catName,
+    color: 'hsl(' + (((index / catNames.length) + 0.0625) * 360) + ', 75%, 60%)',
+    x: 100 + (50 * index),
+    y: 100,
+    scale: 0.3 + (0.1 * index)
+  })
+})
 
 var drawScene = function () {
   context.clearRect(0, 0, canvas.width, canvas.height);
@@ -49,11 +29,15 @@ var drawScene = function () {
 
 drawScene()
 
-nom.oninput = function () {
-  cats[0].weight = this.value
+nom.addEventListener('input', function (event) {
+  var weight = event.target.value
+  cats.forEach(function (cat, catIndex) {
+    cat.weight = weight
+  })
 
   drawScene()
-}
+})
+
 canvas.addEventListener('mousemove', function (event) {
   cats[0].x = event.offsetX
   cats[0].y = event.offsetY
