c2d295123d59c9e4722ca7180470e45bf3e9110e
bowtothetallone <kathy@carlston.tv>
2019-06-28 22:25:06 -0600

    Defined Cat attributes through array

--- cat-image.js
+++ cat-image.js
@@ -2,39 +2,61 @@ var nom = document.getElementById('nomNumber')
 var canvas = document.getElementById('cat-canvas')
 var context = canvas.getContext('2d')
 
-var catA = new Cat({
-  name: 'Grover',
-  color: 'orange',
-  x: 100,
-  y: 100,
-  scale: 0.3
-})
-var catB = new Cat({
-  name: 'Baxter',
-  color: 'Blue',
-  x: 200,
-  y: 100,
-  scale: 0.4
-})
+var cats = [
+  new Cat({
+    name: 'Grover',
+    color: 'red',
+    x: 100,
+    y: 100,
+    scale: 0.3
+  }),
+  new Cat({
+    name: 'Baxter',
+    color: 'orange',
+    x: 200,
+    y: 100,
+    scale: 0.4
+  }),
+  new Cat({
+    name: 'Bruce',
+    color: 'yellow',
+    x: 300,
+    y: 100,
+    scale: 0.5
+  }),
+  new Cat({
+    name: 'Janet',
+    color: 'green',
+    x: 400,
+    y: 100,
+    scale: 0.6
+  }),
+  new Cat({
+    name: 'Admiral',
+    color: 'Blue',
+    x: 500,
+    y: 100,
+    scale: 0.7
+  })
+]
 
 var drawScene = function () {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  catB.draw(context)
-  catA.draw(context)
+  cats.forEach(function (cat, catIndex) {
+    cat.draw(context)
+  })
 }
 
 drawScene()
 
-console.log(catA, catB)
-
 nom.oninput = function () {
-  catA.weight = this.value
+  cats[0].weight = this.value
 
   drawScene()
 }
 canvas.addEventListener('mousemove', function (event) {
-  catA.x = event.offsetX
-  catA.y = event.offsetY
+  cats[0].x = event.offsetX
+  cats[0].y = event.offsetY
 
   drawScene()
 })
